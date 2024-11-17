2ea65b8844a76751820723e3896bfb1fb4643b6c
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-11-17 23:40:00 +0200

    o

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -23,3 +23,32 @@ class Librarian(models.Model):
     def _str_(self):
         return self.name            
 # Create your models here.
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Define user roles
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
+
+# UserProfile model
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+    def _str_(self):
+        return f"{self.user.username} ({self.role})"
+
+# Signal to automatically create a UserProfile
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,4 +24,12 @@ urlpatterns = [
     path('register/', register, name='register'),
 ]
 
-views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name=
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
+]
+#views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name=
