85991728e7c730905a4322517d6edf25ddccfd03
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-11-17 23:45:30 +0200

    o

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -68,3 +68,9 @@ def is_admin(user):
         return user.profile.role == 'Admin'
     except AttributeError:
         return False
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html', {'role': 'Admin'})    
