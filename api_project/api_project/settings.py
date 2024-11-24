855b6f9ec03947a313245cbc0aeb8b32a5fb6cd0
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-11-24 23:59:35 +0200

    ll

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',rest_framework.authtoken
+    'api',rest_framework.authtoken rest_framework.authentication.TokenAuthentication
 ]
 
 MIDDLEWARE = [
