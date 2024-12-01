41f519fa99e8f5020554f21a6fc4478626084830
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-12-01 21:50:46 +0200

    pp

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-class Author(models.Model):
-    name = models.CharField(max_length=255)
 
-    def _str_(self):
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
-    def _str_(self):
-        return self.title
-# Create your models here.
+    def __str__(self):
+        return self.titl
--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -4,17 +4,11 @@ from .models import Author, Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '_all_'
-
-    def validate_publication_year(self, value):
-        from datetime import datetime
-        if value > datetime.now().year:
-            raise serializers.ValidationError("Publication year cannot be in the future.")
-        return value
+        fields = '__all__'
 
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
-        fields = ['name', 'books']
+        fields = ['id', 'name', 'books']
